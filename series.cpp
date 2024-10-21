29e4bcab50b61dbd9e234816bc71295166486a07
nazar <nazar.bojko.2004@gmail.com>
2024-10-21 23:52:43 +0300

    Hotfix: sum of first 3 element

--- series.cpp
+++ series.cpp
@@ -2,5 +2,11 @@
 #include "series.h"
 
 double Series::FuncA() {
-    return 0;
+    double sum = 0.0;
+    double x = 1.0;
+    for (int i = 1; i <= 3; ++i) {
+        sum += (i % 2 == 1 ? 1 : -1) * (pow(x, i) / i);
+    }
+    return sum;
 }
+
